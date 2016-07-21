9f1268a9ad0c08ea7096b5e2fd3e205dd91310f2
Kevin Tran <kevin.tran@finzsoft.com>
2016-07-21 16:10:54 +1200

    Add Leaflet and Google maps

--- src/main/webapp/app/home/home.controller.js
+++ src/main/webapp/app/home/home.controller.js
@@ -20,6 +20,112 @@
 
         getAccount();
 
+        // MAP
+        var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+            //attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+            maxZoom: 18,
+            id: 'pelewin.0n618hc9',
+            accessToken: 'pk.eyJ1IjoicGVsZXdpbiIsImEiOiJjaXF1MGs0dTYwMDlkZndudGc4bHk1aGlxIn0.awdybHhZmOfGzJhZArZYtg'
+        }).addTo(mymap);
+
+        var marker = L.marker([51.5, -0.09]).addTo(mymap);
+
+        var circle = L.circle([51.508, -0.11], 500, {
+            color: 'red',
+            fillColor: '#f03',
+            fillOpacity: 0.5
+        }).addTo(mymap);
+
+        var polygon = L.polygon([
+            [51.509, -0.08],
+            [51.503, -0.06],
+            [51.51, -0.047]
+        ]).addTo(mymap);
+
+        marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+        circle.bindPopup("I am a circle.");
+        polygon.bindPopup("I am a polygon.");
+
+        var popup = L.popup()
+            .setLatLng([51.5, -0.09])
+            .setContent("I am a standalone popup.")
+            .openOn(mymap);
+
+        function onMapClick(e) {
+            popup
+                .setLatLng(e.latlng)
+                .setContent("You clicked the map at " + e.latlng.toString())
+                .openOn(mymap);
+        }
+
+        mymap.on('click', onMapClick);
+
+
+        // END MAP
+
+
+        //GOOGLE MAP
+        function initMap() {
+            var auckland = {lat: -42.86, lng: 174.82};
+            var indianapolis = {lat: 39.79, lng: -86.14};
+
+            var map = new google.maps.Map(document.getElementById('map'), {
+                center: auckland,
+                scrollwheel: false,
+                zoom: 4
+            });
+
+            var markers = [];
+            loadTracks(map, markers);
+
+            var options = {
+                imagePath: 'content/images/m'
+            };
+
+            var markerCluster = new MarkerClusterer(map, markers, options);
+
+
+            /*var i = new google.maps.Marker({
+                position: new google.maps.LatLng(-43.214362, 170.798802),
+                map: map,
+                title: "dfdfd"
+                //,
+                //icon: getIconURL(e.category)
+            });*/
+            /*addTrackMarker({
+                category: "Tramping Track",
+                id: 1821,
+                lat: -43.214362,
+                length: 8110.23150522782,
+                lng: 170.798802,
+                name: "Wanganui Cableway To Smyth Hut",
+                to_param: "1821-wanganui-cableway-to-smyth-hut"
+            });*/
+
+            //initMap();
+            /*var directionsDisplay = new google.maps.DirectionsRenderer({
+                map: map
+            });
+
+            // Set destination, origin and travel mode.
+            var request = {
+                destination: indianapolis,
+                origin: chicago,
+                travelMode: google.maps.TravelMode.DRIVING
+            };
+            // Pass the directions request to the directions service.
+            var directionsService = new google.maps.DirectionsService();
+            directionsService.route(request, function(response, status) {
+                if (status == google.maps.DirectionsStatus.OK) {
+                    // Display the route on the map.
+                    directionsDisplay.setDirections(response);
+                }
+            });*/
+        }
+        initMap();
+        //END GOOGLE MAP
+
         function getAccount() {
             Principal.identity().then(function(account) {
                 vm.account = account;
